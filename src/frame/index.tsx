6433844376a2e323a966a82145587a30b2bce798
aadhav3n <aadhavan3853@gmail.com>
2026-01-14 13:54:47 +0000

    V1.4

--- src/frame/index.tsx
+++ src/frame/index.tsx
@@ -7,23 +7,34 @@ export function Frame() {
         <h1 className={styles.frame__title}>MEGA MAALAI</h1>
         <p className={styles.frame__subtitle}>32 years in the making</p>
       </div>
-      <a className={styles.frame__back} href="https://tympanus.net/codrops/?p=106679">
-        Article
+      <a className={styles.frame__home} href="https://home.megamaalai.org">
+        HOME.MEGAMAALAI.ORG
       </a>
-      <a className={styles.frame__archive} href="https://tympanus.net/codrops/hub/">
-        All demos
+      <a className={styles.frame__timeline} href="https://timeline.megamaalai.org">
+        TIMELINE.MEGAMAALAI.ORG
       </a>
-      <a className={styles.frame__github} href="https://github.com/edoardolunardi/infinite-canvas">
-        GitHub
+      <a className={styles.frame__teams} href="https://teams.megamaalai.org">
+        TEAMS.MEGAMAALAI.ORG
+      </a>
+      <a className={styles.frame__tickets} href="https://tickets.megamaalai.org">
+        TICKETS.MEGAMAALAI.ORG
+      </a>
+      <a className={styles.frame__contact} href="https://www.instagram.com/megamaalai/" target="_blank" rel="noopener noreferrer">
+        @MEGAMAALAI ON INSTAGRAM OR TAMIL@IC.AC.UK
       </a>
       <div className={styles.frame__credits}>
-        <span>By </span>
-        <a href="https://www.edoardolunardi.dev/">Edoardo Lunardi</a>
+        <span>By inhouse</span>
       </div>
       <nav className={styles.frame__tags}>
-        <a href="#">#megamaalai</a>
-        <a href="#">#mm26</a>
-        <a href="#">#imperialtsoc</a>
+        <a href="https://www.instagram.com/explore/tags/megamaalai/" target="_blank" rel="noopener noreferrer">
+          #megamaalai
+        </a>
+        <a href="https://www.instagram.com/explore/tags/mm26/" target="_blank" rel="noopener noreferrer">
+          #mm26
+        </a>
+        <a href="https://www.instagram.com/explore/tags/imperialtsoc/" target="_blank" rel="noopener noreferrer">
+          #imperialtsoc
+        </a>
       </nav>
     </header>
   );
